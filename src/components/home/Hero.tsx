af8e8d033945a6b2706e074d379872a985daeb3f
kuluruvineeth <kuluruvineeth8623@gmail.com>
2023-06-25 12:01:55 +0530

    - Till Demo component completed

---
+++ src/components/home/Hero.tsx
@@ -0,0 +1,64 @@
+import { Box, Button, Flex, SimpleGrid, VStack } from "@chakra-ui/react";
+import Link from "next/link";
+import { HiArrowRight } from "react-icons/hi";
+const Hero = () => {
+  return (
+    <Box px={{ base: 2, md: 6 }}>
+      <SimpleGrid
+        as="main"
+        minHeight="30rem"
+        flex="1"
+        flexDirection="column"
+        marginX="auto"
+        maxWidth="container.lg"
+        columns={{ base: 1, lg: 2 }}
+        px={{ base: 4, lg: 0 }}
+        py={{ base: 10, lg: 0 }}
+        gap={{ base: 10, lg: 0 }}
+      >
+        <VStack
+          className="color"
+          alignItems={{ base: "center", sm: "flex-start" }}
+          spacing={10}
+          justifyContent="center"
+          flexDirection="column"
+        >
+          <Box textAlign={{ base: "center", sm: "left" }}>
+            <Box
+              mb={3}
+              as="h1"
+              maxWidth="43rem"
+              lineHeight={{ base: "2.6rem", sm: "4rem" }}
+              fontSize={{ base: "2.6rem", sm: "4rem" }}
+              fontWeight="black"
+            >
+              Your AI avatar generator
+            </Box>
+            <Box
+              as="h2"
+              maxWidth="30rem"
+              fontSize={{ base: "xl", sm: "3xl" }}
+              lineHeight={{ base: "xl", sm: "3xl" }}
+            >
+              <b>Generate avatars</b> that perfectly capture{" "}
+              <b>your unique style</b>
+            </Box>
+          </Box>
+          <Button
+            as={Link}
+            href=""
+            variant="brand"
+            size="lg"
+            shadow="xl"
+            rightIcon={<HiArrowRight />}
+          >
+            Start Creating Now
+          </Button>
+        </VStack>
+        <Flex alignItems="center">The Demo Component has to be used here</Flex>
+      </SimpleGrid>
+    </Box>
+  );
+};
+
+export default Hero;
---
+++ src/styles/theme.ts
@@ -0,0 +1,72 @@
+import { extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  colors: {
+    brand: {
+      50: "#FFFFFF",
+      100: "#FFFFFF",
+      200: "#FFFFFF",
+      300: "#FFFFFF",
+      400: "#DEFFEE",
+      500: "#B5FFD9",
+      600: "#7DFFBC",
+      700: "#45FF9F",
+      800: "#0DFF83",
+      900: "#00D467",
+    },
+    beige: {
+      50: "#D7D5D4",
+      100: "#CDCACA",
+      200: "#B9B6B5",
+      300: "#A5A1A0",
+      400: "#918D8B",
+      500: "#7D7876",
+      600: "#605C5B",
+      700: "#434140",
+      800: "#262524",
+      900: "#0A0909",
+    },
+  },
+  styles: {
+    global: {
+      body: {
+        bg: "#faf6f5",
+      },
+    },
+  },
+  fonts: {},
+  components: {
+    Button: {
+      variants: {
+        brand: {
+          transition: "all 0.2s",
+          bg: "brand.500",
+          color: "blackAlpha.700",
+          shadow: "lg",
+          borderWidth: "1px",
+          borderColor: "blackAlpha.100",
+          _hover: {
+            shadow: "md",
+          },
+        },
+      },
+    },
+    Link: {
+      variants: {
+        brand: {
+          transition: "all 0.2s",
+          bg: "brand.500",
+          color: "blackAlpha.700",
+          shadow: "lg",
+          borderWidth: "1px",
+          borderColor: "blackAlpha.100",
+          _hover: {
+            shadow: "md",
+          },
+        },
+      },
+    },
+  },
+});
+
+export default theme;
